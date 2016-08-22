f8b33a57cde20648433f9354d82785d0b5592665
Greg Smith <gsmith@theelectricbear.com>
2016-08-22 10:13:51 -0700

    first build norris bot

--- bin/bot.js
+++ bin/bot.js
@@ -1,31 +1,15 @@
-#!/usr/bin/env node
-
 'use strict';
-
-/**
- * NorrisBot launcher script.
- *
- * @author Luciano Mammino <lucianomammino@gmail.com>
- */
 
 var NorrisBot = require('../lib/norrisbot');
 
-/**
- * Environment variables used to configure the bot:
- *
- *  BOT_API_KEY : the authentication token to allow the bot to connect to your slack organization. You can get your
- *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
- *  BOT_DB_PATH: the path of the SQLite database used by the bot
- *  BOT_NAME: the username you want to give to the bot within your organisation.
- */
-var token = process.env.BOT_API_KEY || require('../token');
+var token = 'xoxb-70849268976-A9uo5Cc9dzAOeF0puqsQjw3N';
 var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
 var norrisbot = new NorrisBot({
-    token: token,
-    dbPath: dbPath,
-    name: name
+  token: token,
+  dbPath: dbPath,
+  name: name
 });
 
 norrisbot.run();
